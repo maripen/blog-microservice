2dc921364e2494180041ff08a89c5a477c92c723
maripen <maripen@gmail.com>
2021-11-22 20:36:37 +0100

    add event bus implementation add query service added inter service communication update react client to use query service

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -7,7 +7,7 @@ const PostList = () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get("http://localhost:4000/posts");
+        const res = await axios.get("http://localhost:4002/posts");
 
         setPosts(res.data);
     };
@@ -24,8 +24,8 @@ const PostList = () => {
                 key={post.id}
             >
                 <div className="card-body">
-                    <h3>{post.title}a</h3>
-                    <CommentList postId={post.id} />
+                    <h3>{post.title}</h3>
+                    <CommentList comments={post.comments} />
                     <CommentCreate postId={post.id} />
                 </div>
             </div>
